a58b30a480d8aec8ac55587a8d4d2f134f82ba98
aaqib-webelight <113879497+aaqib-webelight@users.noreply.github.com>
2022-11-08 16:45:24 +0530

    hello
    uu

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -19,7 +19,7 @@ import Stats from '../pages/Stats';
 const pages = [
   {
     route: '/',
-    heading: 'About this siteeeeeeeeeeee',
+    heading: 'About this pages',
     component: Index,
   },
   {
